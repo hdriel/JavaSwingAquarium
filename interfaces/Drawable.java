7af3dd46d63d3562bdc9abc8f6c6f861436dbbaa
Hadriel Benjo <hdriel@gmail.com>
2018-04-22 04:01:50 +0300

    Update Drawable.java

--- interfaces/Drawable.java
+++ interfaces/Drawable.java
@@ -3,7 +3,6 @@ package interfaces;
 import java.awt.Color;
 import java.awt.Graphics;
 
-//Hadriel Benjo 302185061 , campus: Be'er Sheva
 
 public interface Drawable {
 	 public void drawAnimal(Graphics g);
